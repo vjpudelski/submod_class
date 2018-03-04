837ca58be94c6ac319544bdfd773be523f551e2f
Victor J. Pudelski <vjpudelski@gmail.com>
2018-03-03 22:21:23 -0500

    added subtract

--- Math.cs
+++ Math.cs
@@ -5,5 +5,6 @@ namespace submod_class
     public static class Math
     {
         public static int Add(int a, int b) => a + b;
+        public static int Subtract(int a, int b) => a - b;
     }
 }
